261f0782a30af03e3e4e38e1cf17efa4d4207783
Ruth146 <Ruth.abfanossie@gmail.com>
2025-08-01 10:47:14 +0300

    Create bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance  # Private attribute
+
+    def deposit(self, amount):
+        """Add amount to the account balance."""
+        self.__account_balance += amount
+
+    def withdraw(self, amount):
+        """Withdraw amount if sufficient funds are available."""
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        """Print the current account balance."""
+        print(f"Current Balance: ${self.__account_balance}")
